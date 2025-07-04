99bf8edd2b322fd8fdc88fe64f58aac3104e8fd3
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-04 17:15:04 +0200

    Added random quote generator with helper array

---
+++ quotes.js
@@ -0,0 +1,5 @@
+export const quotes = [
+  "Life is what happens when you're busy making other plans.",
+  "Code is like humor. When you have to explain it, it’s bad.",
+  "In the middle of difficulty lies opportunity."
+];
