fb6e9738474e415c1f7dd32693f6a3ce1e1d915c
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-07 21:58:49 +0200

    callback function implemented with settimeout

---
+++ test/ErrorfirstPattern.js
@@ -0,0 +1,16 @@
+function doAsyncTask(callback) {
+  const error = Math.random() > 0.5 ? new Error("Something went wrong") : null;
+  const result = "Task completed";
+
+  setTimeout(() => {
+    callback(error, result);
+  }, 1000);
+}
+
+doAsyncTask((err, result) => {
+  if (err) {
+    console.error("Error:", err.message);
+  } else {
+    console.log("Success:", result);
+  }
+});
