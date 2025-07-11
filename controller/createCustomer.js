fd0973e21c9be0884d26646ebbd232993e9447cd
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-11 23:20:32 +0200

    created customer controller

---
+++ controller/createCustomer.js
@@ -0,0 +1,12 @@
+const { Customer } = require('../models');
+
+exports.createCustomer = async (req, res) => {
+    const { name, email, phone } = req.body;
+
+    try {
+        const customer = await Customer.create({ name, email, phone });
+        res.status(201).json({ message: 'customer created', customer });
+    } catch (error) {
+        console.log('error occurred', error);
+    }
+};
