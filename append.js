beff04b9ae0a9efccaf4efcdc05b1e903d16e968
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-12 22:23:03 +0200

    Deleted Append sytem module

--- append.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const moreContent = '\nAppended this line using appendFile and callback.';
-
-fs.appendFile('source.txt', moreContent, (err) => {
-  if (err) return console.error('Error appending to file:', err);
-  console.log('Content appended successfully.');
-});
