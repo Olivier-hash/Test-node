75b372145429db7998cc95edf3f78c871afee422
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-12 22:24:23 +0200

    Deleted EmojiColicker function

--- Emojipicker.js
+++
@@ -1,7 +0,0 @@
-import { emojis } from './emojiList.js';
-
-function getRandomEmoji() {
-  return emojis[Math.floor(Math.random() * emojis.length)];
-}
-
-console.log("Today's emoji is:", getRandomEmoji());
