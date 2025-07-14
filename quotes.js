35cb0c6644fbe82c3551392b34d7b930eca1979c
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-14 20:38:12 +0200

    Edited user module file

--- quotes.js
+++
@@ -1,5 +0,0 @@
-export const quotes = [
-  "Life is what happens when you're busy making other plans.",
-  "Code is like humor. When you have to explain it, it’s bad.",
-  "In the middle of difficulty lies opportunity."
-];
