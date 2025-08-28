6147605aaa744b576ab86ba303e4c96fd3d029ac
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-28 21:37:48 +0200

    Added order creation logic with basic error logging

--- controller/createOrder.js
+++ controller/createOrder.js
@@ -4,7 +4,7 @@ exports.createOrder = async (req, res) => {
     const { customerId, orderDate } = req.body;
 
     try {
-        const order = await Order.create({ customerId, orderDate, status });
+        const orders = await Order.create({ customerId, orderDate, status });
         res.status(201).json({ message: 'order created manually', order });
     } catch (error) {
         console.log('error occurred', error);
