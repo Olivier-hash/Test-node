b8df64851491f056cbc566a8a196ae1659701aea
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-28 21:38:49 +0200

    implemented createOrder controller

--- controller/createCustomer.js
+++ controller/createCustomer.js
@@ -1,7 +1,7 @@
 const { Customer } = require('../models');
 
 exports.createCustomer = async (req, res) => {
-    const { name, email, phone } = req.body;
+    const { name, email } = req.body;
 
     try {
         const customer = await Customer.create({ name, email, phone });
