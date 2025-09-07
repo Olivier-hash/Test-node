2157ae220bc4fde0b4d242446e02417a5707fb6a
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-07 22:03:10 +0200

    reorganized database connection logic in db.php

--- controller/createOrder.js
+++ controller/createOrder.js
@@ -1,10 +1,12 @@
 const { Order } = require('../models');
 
 exports.createOrder = async (req, res) => {
+
+    
     const {   } = req.body;
 
     try {
-        const orders = await Order.create({ customerId, orderDate, status });
+        const order = await Order.create({ customerId, orderDate, status });
         res.status(201).json({ message: 'order created manually', order });
     } catch (error) {
         console.log('error occurred', error);
