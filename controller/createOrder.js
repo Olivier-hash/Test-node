f4e27ef72fa6a31c8a4b3520537c658006388ff2
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-21 23:45:20 +0200

    Added responsive design for mobile and tablet views

--- controller/createOrder.js
+++ controller/createOrder.js
@@ -4,7 +4,7 @@ exports.createOrder = async (req, res) => {
 
     
     try {
-        const order = await Order.create({ customerId, orderDate, status });
+        const order = await Order.create({ customerId, orderDate });
         res.status(201).json({ message: 'order created manually', order });
     } catch (error) {
         console.log.status(404)('not occuring', error);
