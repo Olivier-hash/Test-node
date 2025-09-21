0c7a5c904c89ce14f81639096147cc97f8ec8d7e
olivier Rukundo <rr.olivier01@gmail.com>
2025-09-21 23:43:59 +0200

    Implemented real-time chat notifications using WebSockets

--- controller/createOrder.js
+++ controller/createOrder.js
@@ -3,8 +3,6 @@ const { Order} = require('../models');
 exports.createOrder = async (req, res) => {
 
     
-    const {   } = req.body;
-
     try {
         const order = await Order.create({ customerId, orderDate, status });
         res.status(201).json({ message: 'order created manually', order });
